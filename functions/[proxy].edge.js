6a05efddc106386fb3aed382161f76d53dca22d6
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-10 19:04:17 +0530

    chnaged edge fn

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,10 +1,24 @@
-export default function handler(request,context) {
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
-    console.log(route);
-    if (route === '/test' && request.method === 'GET') {
-     return fetch(`https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge`);
-    }
-    return fetch(request)
-   }
-   
+// export default function handler(request,context) {
+//     const parsedUrl = new URL(request.url);
+//     const route = parsedUrl.pathname;
+//     console.log(route);
+//     if (route === '/test' && request.method === 'GET') {
+//      return fetch(`https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge`);
+//     }
+//     return fetch(request)
+//    }
+export default {
+    async fetch(request, context) {
+      const parsedUrl = new URL(request.url);
+      const route = parsedUrl.pathname;
+      
+      console.log(route); // Logs the requested route
+  
+      if (route === "/test" && request.method === "GET") {
+        return await fetch("https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge");
+      }
+  
+      return await fetch(request);
+    },
+  };
+  
