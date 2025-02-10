e9a302dc54d0bc6128b45fccd53fd085f0526300
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-10 18:53:44 +0530

    modified the edge fn

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,6 +1,7 @@
-export default function handler(request) {
+export default function handler(request,context) {
     const parsedUrl = new URL(request.url);
     const route = parsedUrl.pathname;
+    console.log(route);
     if (route === '/test' && request.method === 'GET') {
      return fetch(`https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge`);
     }
