906d6812541b51ee0475f45af4db59b159657dff
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-10 23:38:28 +0530

    modified handler

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -13,28 +13,14 @@
 //     console.log(error);
 //   }
 // }
-export default 
-    async function handler(request, context) {
-      try {
-        const parsedUrl = new URL(request.url);
-        const route = parsedUrl.pathname;
-  
-        console.log(route); // Log the requested route
-  
-        if (route === "/test" && request.method === "GET") {
-          return await fetch(
-            "https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge"
-          );
-        }
-  
-        return await fetch(request);
-      } catch (error) {
-        console.error("Error handling request:", error);
-  
-        return new Response(JSON.stringify({ message: "Internal Server Error" }), {
-          status: 500,
-          headers: { "Content-Type": "application/json" },
-        });
-      }
-    };
-  
+
+export default async function handler(request) {
+    const modifiedUrl = new URL(request.url);
+    const route = modifiedUrl.pathname;
+    if (route === '/test' && request.method === 'GET') {
+      modifiedUrl.pathname = '/contact';
+      return Response.redirect(modifiedUrl, 301)
+    }
+    return fetch(request);
+   }
+   
