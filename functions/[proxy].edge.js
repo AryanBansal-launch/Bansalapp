594dbfd3b37e7b0f92e650e6d99aeef2076fb253
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-11 10:45:51 +0530

    changed edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,26 +1,9 @@
-// export default async function handler(request, context) {
-//   try {
-//     const parsedUrl = new URL(request.url);
-//     const route = parsedUrl.pathname;
-//     console.log(route);
-//     if (route === "/test" && request.method === "GET") {
-//       return fetch(
-//         `https://www.contentstack.com/docs/developers/launch/edge-functions#handling-routes-at-edge`
-//       );
-//     }
-//     return fetch(request);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
 export default async function handler(request) {
-    const modifiedUrl = new URL(request.url);
-    const route = modifiedUrl.pathname;
-    if (route === '/test' && request.method === 'GET') {
-      modifiedUrl.pathname = '/contact';
-      return Response.redirect(modifiedUrl, 301)
-    }
-    return fetch(request);
-   }
-   
+  const modifiedUrl = new URL(request.url);
+  const route = modifiedUrl.pathname;
+  if (route === "/test" && request.method === "GET") {
+    modifiedUrl.pathname = "/contact";
+    return Response.redirect(modifiedUrl, 301);
+  }
+  return fetch(request);
+}
