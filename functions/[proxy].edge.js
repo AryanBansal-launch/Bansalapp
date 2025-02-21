9a160ef25e8a1a3f87ae0c1e8db679b59a4dbc67
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-21 15:31:25 +0530

    removed edge fn code

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,10 +1,10 @@
-//edge function for redirection demonstration, the url is not re-written, the page that is rendered is of /contact but url is also re-written to /contact instead of /test
-export default async function handler(request) {
-  const modifiedUrl = new URL(request.url);
-  const route = modifiedUrl.pathname;
-  if (route === "/test" && request.method === "GET") {
-    modifiedUrl.pathname = "/contact";
-    return Response.redirect(modifiedUrl, 301);
-  }
-  return fetch(request);
-}
+// // edge function for redirection demonstration, the url is not re-written, the page that is rendered is of /contact but url is also re-written to /contact instead of /test
+// export default async function handler(request) {
+//   const modifiedUrl = new URL(request.url);
+//   const route = modifiedUrl.pathname;
+//   if (route === "/test" && request.method === "GET") {
+//     modifiedUrl.pathname = "/contact";
+//     return Response.redirect(modifiedUrl, 301);
+//   }
+//   return fetch(request);
+// }
