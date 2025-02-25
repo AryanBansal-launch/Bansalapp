9da398ccb0f3af99ffc9b1109859b4c64bc2a856
Aryan Bansal <aryan.bansal@contentstack.com>
2025-02-25 17:33:17 +0530

    trying redirects

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,7 +2,7 @@
 export default async function handler(request) {
   const modifiedUrl = new URL(request.url);
   const route = modifiedUrl.pathname;
-  if (route === "/projects" && request.method === "GET") {
+  if (route === "/test2" && request.method === "GET") {
     modifiedUrl.pathname = "/contact";
     return Response.redirect(modifiedUrl, 301);
   }
