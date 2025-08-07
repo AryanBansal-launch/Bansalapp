62dd3b49f0e8571121c6f4f47381296ceb43c98a
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-08 00:12:18 +0530

    added permanent url

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   const imagePath = "v3/assets/blt41b5d34d676c7949/blt12253853eb06f352/project thumbnail.png";
   return (
     <div>
-      this is test page
+      this is test page having image from contentstack assets permanent url
       <img src={imagePath} alt="test" />
     </div>
   )
