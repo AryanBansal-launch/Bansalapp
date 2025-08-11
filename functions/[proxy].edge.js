ae7861f508d85d0208a2726b268c07e7ba3d0e1d
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-11 16:22:28 +0530

    reverted edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,52 +1,52 @@
 //edge function to fix the RSC issue
-export default async function handler(request) {
-  const parsedUrl = new URL(request.url);
-  const route = parsedUrl.pathname;
+// export default async function handler(request) {
+//   const parsedUrl = new URL(request.url);
+//   const route = parsedUrl.pathname;
   
   
-  if (route === '/example') {
-    return await example_edge_function(request);
-  }
+//   if (route === '/example') {
+//     return await example_edge_function(request);
+//   }
   
-  // For all other routes, use RSC function
-  return await rsc_edge_function(request);
-}
+//   // For all other routes, use RSC function
+//   return await rsc_edge_function(request);
+// }
 
-//1. Edge function to fix the RSC issue
-const rsc_edge_function = async (request) => {
-  const RSC_REDIRECT_PATHS = [
-    '/about','/projects','/skills'
-  ];
-  const RSC_HEADER = 'Rsc';
-  const RSC_HEADER_VALUE = '1';
-  const RSC_QUERY_PARAM = 'rsc';
-    console.log("This is an edge functions test");
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
-    const rscQueryParamExists = !!parsedUrl.searchParams.get(RSC_QUERY_PARAM);
-    const rscHeaderExists = request.headers.get(RSC_HEADER) === RSC_HEADER_VALUE;
-    const isRscRedirectPath = RSC_REDIRECT_PATHS.indexOf(route) > -1;
+// //1. Edge function to fix the RSC issue
+// const rsc_edge_function = async (request) => {
+//   const RSC_REDIRECT_PATHS = [
+//     '/about','/projects','/skills'
+//   ];
+//   const RSC_HEADER = 'Rsc';
+//   const RSC_HEADER_VALUE = '1';
+//   const RSC_QUERY_PARAM = 'rsc';
+//     console.log("This is an edge functions test");
+//     const parsedUrl = new URL(request.url);
+//     const route = parsedUrl.pathname;
+//     const rscQueryParamExists = !!parsedUrl.searchParams.get(RSC_QUERY_PARAM);
+//     const rscHeaderExists = request.headers.get(RSC_HEADER) === RSC_HEADER_VALUE;
+//     const isRscRedirectPath = RSC_REDIRECT_PATHS.indexOf(route) > -1;
   
-    if (isRscRedirectPath && !rscQueryParamExists && rscHeaderExists) {
-        const modifiedRequest = new Request(request);
-        modifiedRequest.headers.delete(RSC_HEADER);
-        return fetch(modifiedRequest);
-    }
-    return fetch(request);
-  }
+//     if (isRscRedirectPath && !rscQueryParamExists && rscHeaderExists) {
+//         const modifiedRequest = new Request(request);
+//         modifiedRequest.headers.delete(RSC_HEADER);
+//         return fetch(modifiedRequest);
+//     }
+//     return fetch(request);
+//   }
 
-  //2. Edge function to handle the example route
-  const example_edge_function = async (request) => {
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
-    if (route === '/example') {
-      const response = {
-        time: new Date()
-      }
-      return new Response(JSON.stringify(response))
-    }
-    return fetch(request)
-   }
+//   //2. Edge function to handle the example route
+//   const example_edge_function = async (request) => {
+//     const parsedUrl = new URL(request.url);
+//     const route = parsedUrl.pathname;
+//     if (route === '/example') {
+//       const response = {
+//         time: new Date()
+//       }
+//       return new Response(JSON.stringify(response))
+//     }
+//     return fetch(request)
+//    }
 
 
 // const CONTENTSTACK_CDN_HOST = 'https://images.contentstack.io';
@@ -136,33 +136,33 @@ const rsc_edge_function = async (request) => {
 // }
 
 //chatgpt suggested way
-// export default async function handler(request) {
-//   const url = new URL(request.url);
-//   const pathname = url.pathname;
+export default async function handler(request) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
 
-//   if (pathname.includes('/v3/assets')) {
-//     console.log("Proxying Contentstack asset:", pathname);
+  if (pathname.includes('/v3/assets')) {
+    console.log("Proxying Contentstack asset:", pathname);
 
-//     const newreqHeaders = new Headers(request.headers);
-//     newreqHeaders.set('Cache-Control', 'no-store');
+    const newreqHeaders = new Headers(request.headers);
+    newreqHeaders.set('Cache-Control', 'no-store');
 
-//     // Fetch original asset
-//     const originResponse = await fetch('https://images.contentstack.io' + pathname, {
-//       method: request.method,
-//       body: request.body,
-//       headers: newreqHeaders
-//     });
+    // Fetch original asset
+    const originResponse = await fetch('https://images.contentstack.io' + pathname, {
+      method: request.method,
+      body: request.body,
+      headers: newreqHeaders
+    });
 
-//     // Clone response & set custom cache header
-//     const newHeaders = new Headers(originResponse.headers);
-//     newHeaders.set('Cache-Control', 'no-store');
+    // Clone response & set custom cache header
+    const newHeaders = new Headers(originResponse.headers);
+    newHeaders.set('Cache-Control', 'no-store');
 
-//     return new Response(originResponse.body, {
-//       status: originResponse.status,
-//       statusText: originResponse.statusText,
-//       headers: newHeaders
-//     });
-//   }
+    return new Response(originResponse.body, {
+      status: originResponse.status,
+      statusText: originResponse.statusText,
+      headers: newHeaders
+    });
+  }
 
-//   return fetch(request);
-// }
+  return fetch(request);
+}
