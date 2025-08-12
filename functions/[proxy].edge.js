ad4d96e47390349dadffdc2d3a58eb49fd2821dc
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-12 18:55:30 +0530

    test vercel

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -61,5 +61,5 @@
 // }
 
 export default function handler() {
-  return fetch('https://nextjs-cache-test-ecpo9ybdl-aryan-bansals-projects-67cf3cd2.vercel.app/api/test');
+  return fetch('https://nextjs-cache-test-2bvv95tiy-aryan-bansals-projects-67cf3cd2.vercel.app/');
 }
