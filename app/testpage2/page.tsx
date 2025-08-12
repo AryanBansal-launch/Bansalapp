63768dbc445b12c5998abf291ef27b6733221c97
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-12 14:27:13 +0530

    add testpage with full url

---
+++ app/testpage2/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  const imagePath = "https://images.contentstack.io/v3/assets/blt41b5d34d676c7949/blt12253853eb06f352/project thumbnail.png";
+  return (
+    <div>
+      this is test page having image from contentstack assets permanent url
+      <img src={imagePath} alt="test" />
+    </div>
+  )
+}
+
+export default page
