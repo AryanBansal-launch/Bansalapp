603b22f2da5d7fd38205052add98483ae8cb4b49
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-12 18:19:10 +0530

    random api without cache header

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,60 +1,61 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
-export default function handler(request) {
-  // console.log("This is an edge functions test for asset proxy");
-  const url = new URL(request.url);
-  // console.log("URL coming to edge:",url);
-  const pathname = url.pathname;
-  // console.log("Pathname:",pathname);
-  if(pathname.includes('/v3/assets')){
-    // const myhostname = 'https://images.contentstack.io';
-    console.log("This is a Contentstack asset request");
-    console.log("old request:",request.url);
+// export default function handler(request) {
+//   // console.log("This is an edge functions test for asset proxy");
+//   const url = new URL(request.url);
+//   // console.log("URL coming to edge:",url);
+//   const pathname = url.pathname;
+//   // console.log("Pathname:",pathname);
+//   if(pathname.includes('/v3/assets')){
+//     // const myhostname = 'https://images.contentstack.io';
+//     console.log("This is a Contentstack asset request");
+//     console.log("old request:",request.url);
     
-    const newreqHeaders = new Headers(request.headers);
+//     const newreqHeaders = new Headers(request.headers);
     
-    newreqHeaders.set('x-launch-deploymentuid', uuidv4());
-    // newreqHeaders.set('Cache-Control', 'no-store, no-cache, must-revalidate');
-    // newreqHeaders.set('Pragma', 'no-cache');
+//     newreqHeaders.set('x-launch-deploymentuid', uuidv4());
+//     // newreqHeaders.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+//     // newreqHeaders.set('Pragma', 'no-cache');
 
 
-    const newRequest = new Request('https://images.contentstack.io'+ pathname, {
-            redirect: 'follow',
-            cf: {
-                  cacheTtl: 0,
-                  cacheEverything: true,
-                  cacheTtlByStatus: { "200-599": 0 },
-                  bypassCache: true,
-                },
-          });
-          console.log("New request:",newRequest.url);
-          return fetch(newRequest);
+//     const newRequest = new Request('https://images.contentstack.io'+ pathname, {
+//             redirect: 'follow',
+//             cf: {
+//                   cacheTtl: 0,
+//                   cacheEverything: true,
+//                   cacheTtlByStatus: { "200-599": 0 },
+//                   bypassCache: true,
+//                 },
+//           });
+//           console.log("New request:",newRequest.url);
+//           return fetch(newRequest);
   
-    // const originResponse = await fetch('https://images.contentstack.io' + pathname, {
-    //   method: request.method,
-    //   body: request.body,
-    //   headers: newreqHeaders,
-    //   cf: {
-    //     cacheTtl: 0,
-    //     cacheEverything: true,
-    //     cacheTtlByStatus: { "200-599": 0 }
-    //   },
-    // });
+//     // const originResponse = await fetch('https://images.contentstack.io' + pathname, {
+//     //   method: request.method,
+//     //   body: request.body,
+//     //   headers: newreqHeaders,
+//     //   cf: {
+//     //     cacheTtl: 0,
+//     //     cacheEverything: true,
+//     //     cacheTtlByStatus: { "200-599": 0 }
+//     //   },
+//     // });
 
-    // // Clone response & set custom cache header
-    // const newresHeaders = new Headers(originResponse.headers);
-    // newresHeaders.set('Cache-Control', 'no-store, no-cache, must-revalidate');
-    // newresHeaders.set('Pragma', 'no-cache');
+//     // // Clone response & set custom cache header
+//     // const newresHeaders = new Headers(originResponse.headers);
+//     // newresHeaders.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+//     // newresHeaders.set('Pragma', 'no-cache');
 
-    // return new Response(originResponse.body, {
-    //   status: originResponse.status,
-    //   statusText: originResponse.statusText,
-    //   headers: newresHeaders
-    // });
-  }
+//     // return new Response(originResponse.body, {
+//     //   status: originResponse.status,
+//     //   statusText: originResponse.statusText,
+//     //   headers: newresHeaders
+//     // });
+//   }
 
-  return fetch(request);
+//   return fetch(request);
+// }
+
+export default function handler() {
+  return fetch('https://randomuser.me/api/');
 }
-
-
-
