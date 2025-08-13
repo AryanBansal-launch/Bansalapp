327a0a293a6becbea2995eee3f06eb38619e09e9
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-13 12:12:39 +0530

    added api route with CMS call

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,13 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    // Handle GET request - return some data or status
-    return NextResponse.json({ 
-      success: true, 
-      message: "Data API endpoint is working", 
+    const response = await fetch(`https://cdn.contentstack.io/v3/content_types/common_page/entries/bltbd861b1a14f9288e`,
+    {
       method: "GET",
-      timestamp: new Date().toISOString()
-    });
+      headers: {
+        "Content-Type": "application/json",
+        "api_key": process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+        "access_token": process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
+      }, 
+    })
+    // Handle GET request - return some data or status
+    // return NextResponse.json({ 
+    //   success: true, 
+    //   message: "Data API endpoint is working", 
+    //   method: "GET",
+    //   timestamp: new Date().toISOString()
+    // });
+    return response;
   } catch (error) {
     console.error("Error in GET request:", error);
     return NextResponse.json({ success: false, error: "Internal server error" }, { status: 500 });
