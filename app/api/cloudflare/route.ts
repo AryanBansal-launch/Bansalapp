e3b13834e5a8bf9f3b95cae67448889b498d7229
Aryan Bansal <aryan.bansal@contentstack.com>
2025-08-20 18:47:38 +0530

    add routes to check cache

---
+++ app/api/cloudflare/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSkillsRes } from "@/helper";
+
+export async function GET(req: NextRequest) {
+  try {
+    const q=req.nextUrl.searchParams.get("query") || "";
+    const skills = await getSkillsRes(q,"/skills");
+    const response = NextResponse.json(skills, { status: 200 });
+
+    // Add cache headers (10 minutes)
+    response.headers.set(
+      "Cache-Control",
+      "no-store"
+    );
+
+    return response;
+  } catch (error) {
+    console.error("Error in GET request:", error);
+    return NextResponse.json({ success: false, error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json(); // Parse JSON request body
+    console.log("Received Data:", body);
+
+    // Example: Simulate saving data (replace with actual database logic)
+    return NextResponse.json({ success: true, message: "Data received from API call", data: body });
+  } catch (error) {
+    console.error("Error processing request:", error); // Log the error
+    return NextResponse.json({ success: false, error: "Invalid request" }, { status: 400 });
+  }
+}
--- helper/index.ts
+++ helper/index.ts
@@ -146,7 +146,53 @@ export const getSkillsRes = async (q: string,entryUrl: string): Promise<Page> =>
   return data.entry;
 };
 
+export const getSkillsResCached = async (q: string,entryUrl: string): Promise<Page> => {
+  // const t1 = new Date();
+  const response = await fetch(
+    `https://cdn.contentstack.io/v3/content_types/common_page/entries/bltbd861b1a14f9288e`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "api_key": process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+        "access_token": process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
+      }, 
+    }
+  );
 
+  if (!response.ok) {
+    throw new Error(`Failed to fetch skills data: ${response.statusText}`);
+  }
+  // const t2= new Date();
+  // console.log("Time:", t2.getTime() - t1.getTime(),"x-request-id:", response.headers.get("x-request-id"),"x-cache:", response.headers.get("x-cache"),"query:",q);
+  
+  
+
+  
+  const data = await response.json();
+  // console.log("Skills Data from backend:", data);
+  return data.entry;
+};
+
+export const getSkillsResCloudflare = async (q: string,entryUrl: string): Promise<Page> => {
+  const t1 = new Date();
+  const response = await fetch(
+    `https://bansalappdnd-test.contentstackapps.com/api/cached`,
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch skills data: ${response.statusText}`);
+  }
+  const t2= new Date();
+  console.log("Time from Cloudflare:", t2.getTime() - t1.getTime(),"x-request-id:", response.headers.get("x-request-id"),"x-cache:", response.headers.get("x-cache"),"query:",q);
+  
+  
+
+  
+  const data = await response.json();
+  // console.log("Skills Data from backend:", data);
+  return data.entry;
+};
 
 //for getting About page
 export const getAboutRes = async (entryUrl: string): Promise<Page> => {
