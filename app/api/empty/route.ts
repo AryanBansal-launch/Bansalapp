4fc67dc7bf2716fc0b1f0504cd20c47dcb8dcb94
AryanBansal-launch <aryan.bansal@contentstack.com>
2025-09-02 12:23:15 +0530

    Update route.ts

--- app/api/empty/route.ts
+++ app/api/empty/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log("Origin call");
   const response = NextResponse.json({ success: true, message: "Empty response" }, { status: 200 });
   return response;
 }
