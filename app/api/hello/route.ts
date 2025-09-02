2ecd57216ec8f085cb4bf0a1726c00f9e566b30f
AryanBansal-launch <aryan.bansal@contentstack.com>
2025-09-02 12:24:00 +0530

    Update route.ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,13 +1,1 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  console.log("hello api called");
-  res.status(200).json({ name: "John Doe" });
-}
